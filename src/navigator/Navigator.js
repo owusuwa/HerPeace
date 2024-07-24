af7f1436729175f8988c7189e8c48eb591aeaab2
owusuwa <okameka@st.ug.edu.gh>
2024-07-23 21:07:58 -0700

    added native stack navigator

---
+++ src/navigator/Navigator.js
@@ -0,0 +1,4 @@
+import {createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import Home from '../screens/Home'
+import Details from "../screens/Details";
